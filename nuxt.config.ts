1245b437a9e8f48d73d7d2b35de9704bc53843cb
rizkysafdila <rizkysafdila@gmail.com>
2025-08-12 13:46:42 +0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+import tailwindcss from '@tailwindcss/vite'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@pinia/nuxt',
+  ],
+
+  css: ['~/assets/css/main.css'],
+
+  compatibilityDate: '2025-07-16',
+
+  eslint: {
+    config: {
+      standalone: false,
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.API_BASE_URL,
+    },
+  },
+})
